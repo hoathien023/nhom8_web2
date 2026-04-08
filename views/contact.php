cbe4a60aae2ed220dd419b743bf925a60caf1e76
hoathien023 <hoathien023@gmail.com>
2026-04-08 19:53:31 +0700

    Restore project after XAMPP reinstall

---
+++ views/contact.php
@@ -0,0 +1,65 @@
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb__links">
+                    <a href="./index.html"><i class="fa fa-home"></i> Home</a>
+                    <span>Contact</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb End -->
+
+<!-- Contact Section Begin -->
+<section class="contact spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <div class="contact__content">
+                    <div class="contact__address">
+                        <h5>THÔNG TIN LIÊN LẠC</h5>
+                        <ul>
+                            <li>
+                                <h6><i class="fa fa-map-marker"></i> Địa chỉ</h6>
+                                <p>khu Yên Sơn, Thuỵ Hương, Chương Mỹ, Hà Nội</p>
+                            </li>
+                            <li>
+                                <h6><i class="fa fa-phone"></i> Sô điện thoại</h6>
+                                <p><span>0123456789</span><span>0987654321</span></p>
+                            </li>
+                            <li>
+                                <h6><i class="fa fa-headphones"></i> Hỗ trợ</h6>
+                                <p>hotro@gmail.com</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="contact__form">
+                        <h5>
+                            GỬI TIN NHẮN</h5>
+                        <form action="#">
+                            <input type="text" placeholder="Họ tên">
+                            <input type="text" placeholder="Email">
+                            <input type="text" placeholder="Website">
+                            <textarea placeholder="Tin nhắn"></textarea>
+                            <button type="submit" class="site-btn">GỬI TIN NHẮN</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="contact__map">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3726.9800795185006!2d105.7004607!3d20.913121099999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31344def665bc727%3A0x6b9b39cae8be2b53!2zVHLGsOG7nW5nIFRIUFQgQ2jGsMahbmcgTeG7uSBB!5e0!3m2!1svi!2s!4v1748265593958!5m2!1svi!2s"
+                        height="780" style="border:0" allowfullscreen="">
+                    </iframe>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Section End -->
